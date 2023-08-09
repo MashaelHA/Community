0ab6b74f6751289c0555ee2e0cbb55ea9d88ea7b
MashaelHA <meeshooo1415@gmail.com>
2023-08-09 13:28:57 +0300

    add at package

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,19 @@
     <br>
     <NuxtLink to="hi">Hi page</NuxtLink>
     </h1>
+
+
+  <!-- <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/posts/1">Post 1</NuxtLink></li>
+        <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+      </ul>
+    </nav>
+  </header> -->
+
+
   </div>
 </template>
 
