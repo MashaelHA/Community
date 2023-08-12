2eb3ce2938cf0437b93c3146743817c5ca187427
MashaelHA <meeshooo1415@gmail.com>
2023-08-12 18:14:17 +0300

    add some of data from API to posts page

--- components/posts.vue
+++ components/posts.vue
@@ -1,9 +1,23 @@
 <template>
     <div class="post">
+
+
+        <!-- <h1>{{ appTitle }}</h1>
+        <p>Random Number: {{ getRandomNumber() }}</p>
+        <img :src="getRandomImage(1, 5, 'posts', 'jpg')" alt="Random Image" /> -->
+        <!-- <img src="../assets/images/posts/1.jpg" alt="Random Image" /> -->
+
         <div id="post" v-if="posts.length > 0">
             <h1>Posts:</h1>
             <div v-for="post in posts" :key="post.id">
                 {{ post.title }}
+                <!-- <img :src="getRandomImage(1, 2, 'assets/images/posts', 'jpg')" alt="Random Image" /> -->
+                <img :src="getRandomImage('jpg')" alt="Random Image" />
+
+                <!-- <img :src="post.image" alt="not find"/> -->
+
+                <!-- <img src="../assets/images/posts/25.jpg" alt="Random Image" /> -->
+
             </div>
         </div>
 
@@ -15,66 +29,64 @@
         <!-- <h1>{{ app_title }}</h1>
         <p>Random Number: {{ randomNumber(1, 25) }}</p>
         <img :src="randomImage(1, 5, 'images', 'jpg')" alt="Random Image" /> -->
+
+
+
+
     </div>
 </template>
   
 <script>
 import api from './api.js';
-// import store from './store.js';
-// import { mapGetters } from 'vuex';
-
 export default {
     data() {
         return {
             posts: [],
         }
     },
-    // beforeRouteEnter(to, from, next) {
-    //     next(async ctx => {
-    //         ctx.posts = await api.get('posts');
-    //     });
+    // computed: {
+    //     appTitle() {
+    //         return 'تطبيقي الفخم';
+    //     },
     // },
-    computed: {
-        // ...mapGetters(['app_title', 'randomNumber', 'randomImage']),
-
-        // counter() {
-        //     return this.$store.state.counter;
-        // },
-
-        // ...mapGetters(['getRandomValue']),
-
-        // ...mapGetters(['app_title', 'randomNumber', 'randomImage']),
-    },
-    // methods: {
-    //     ...mapActions(['generateRandomValue']),
-    // },
-
     async fetch() {
         this.posts = await api.get('posts');
     },
-
     async mounted() {
-        const randomNumber = this.generateRandomNumber();
-        console.log(randomNumber);
-        // console.log(this.$store.state.randomNumber())
-        // console.log(this.$store.getters.randomNumber());
+        //     this.posts = await api.get('posts');
+        //     // console.log(posts)
+
+        //     // api.get('posts').then(data => this.posts = data)
+        //     //     .catch(error => {
+        //     //     });
 
 
-        // console.log(this.$store.state.counter)
-    //     // console.log(store.getters)
-    //     this.posts = await api.get('posts');
-    //     // console.log(posts)
-
-    //     // api.get('posts').then(data => this.posts = data)
-    //     //     .catch(error => {
-    //     //     });
-
+        // api.get('posts').then(data => {
+        //     this.posts = data.map(x => ({
+        //         id: x.id,
+        //         title: x.title,
+        //         image: getRandomImage(1, 5, 'posts', 'jpg'),
+        //     }))
+        // })
     },
-
     methods: {
-        generateRandomNumber() {
-            return Math.floor(Math.random() * 25) + 1;
-        }
+        getRandomNumber(start, end) {
+            return Math.floor(Math.random() * (end - start + 1)) + start;
+        },
+        // getImagePath() {
+        //     return require(`@/assets/images/posts`); // Use require to get the correct path
+        // },
+        // getRandomImage(start, end, folder, ext) {
+        //     const randomNumber = Math.floor(Math.random() * (end - start + 1)) + start;
+        //     return `${folder}/${randomNumber}.${ext}`;
+
+        //     // return `/${folder}/${randomNumber}.${ext}`;
+        //     // return "@/assets/images/posts/1.jpg";
+        // },
+        getRandomImage(ext) {
+            const randomNumber = this.getRandomNumber(1, 2);
+            return require(`@/assets/images/posts/${randomNumber}.${ext}`);
+        },
     }
 }
 </script>
