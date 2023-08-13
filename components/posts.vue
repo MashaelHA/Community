3bdda6da290326bde3226129e213ab58c60a9a26
MashaelHA <meeshooo1415@gmail.com>
2023-08-13 21:10:45 +0300

    desine page posts

--- components/posts.vue
+++ components/posts.vue
@@ -1,36 +1,26 @@
 <template>
-    <div class="post">
+    <div class="post container-fluid">
 
-
-        <!-- <h1>{{ appTitle }}</h1>
-        <p>Random Number: {{ getRandomNumber() }}</p>
-        <img :src="getRandomImage(1, 5, 'posts', 'jpg')" alt="Random Image" /> -->
-        <!-- <img src="../assets/images/posts/1.jpg" alt="Random Image" /> -->
-
-        <div id="post" v-if="posts.length > 0">
-            <h1>Posts:</h1>
-            <div v-for="post in posts" :key="post.id">
-                {{ post.title }}
-                <!-- <img :src="getRandomImage(1, 2, 'assets/images/posts', 'jpg')" alt="Random Image" /> -->
-                <img :src="getRandomImage('jpg')" alt="Random Image" />
-
-                <!-- <img :src="post.image" alt="not find"/> -->
-
-                <!-- <img src="../assets/images/posts/25.jpg" alt="Random Image" /> -->
-
+        <div class="row" v-if="posts.length > 0">
+            <div class="col-12 col-md-4 mb-3" v-for="post in posts" :key="post.id">
+                <NuxtLink :to="'/posts/'+post.id" class="text-decoration-none">
+                    <div id="post" class="card">
+                    </div>
+                    <!-- <h1>Posts:</h1> -->
+                    <div class="card-header bg-primary text-white">
+                        {{ post.title.slice(0, 50) }}
+                    </div>
+                    <div class="card-body p-0">
+                        <div class="thumbnail" :style="{ backgroundImage: `url(${getRandomImage('jpg')})` }">
+                            <!-- <img :src="getRandomImage('jpg')" alt="Random Image" class="image-fluid" /> -->
+                        </div>
+                        <div class="card-footer p-2 text-dark">
+                            {{ post.body }}
+                        </div>
+                    </div>
+                </NuxtLink>
             </div>
         </div>
-
-        <!-- <p>Counter: {{ counter }}</p> -->
-        <!--         
-        <h2>Random Value: {{ randomValue }}</h2>
-    <button @click="generateRandomValue">Generate Random Value</button> -->
-
-        <!-- <h1>{{ app_title }}</h1>
-        <p>Random Number: {{ randomNumber(1, 25) }}</p>
-        <img :src="randomImage(1, 5, 'images', 'jpg')" alt="Random Image" /> -->
-
-
 
 
     </div>
@@ -42,51 +32,47 @@ export default {
     data() {
         return {
             posts: [],
+            // imgecard: ,
         }
     },
-    // computed: {
-    //     appTitle() {
-    //         return 'تطبيقي الفخم';
-    //     },
-    // },
     async fetch() {
         this.posts = await api.get('posts');
-    },
-    async mounted() {
-        //     this.posts = await api.get('posts');
-        //     // console.log(posts)
-
-        //     // api.get('posts').then(data => this.posts = data)
-        //     //     .catch(error => {
-        //     //     });
-
-
-        // api.get('posts').then(data => {
-        //     this.posts = data.map(x => ({
-        //         id: x.id,
-        //         title: x.title,
-        //         image: getRandomImage(1, 5, 'posts', 'jpg'),
-        //     }))
-        // })
     },
     methods: {
         getRandomNumber(start, end) {
             return Math.floor(Math.random() * (end - start + 1)) + start;
         },
-        // getImagePath() {
-        //     return require(`@/assets/images/posts`); // Use require to get the correct path
-        // },
-        // getRandomImage(start, end, folder, ext) {
-        //     const randomNumber = Math.floor(Math.random() * (end - start + 1)) + start;
-        //     return `${folder}/${randomNumber}.${ext}`;
-
-        //     // return `/${folder}/${randomNumber}.${ext}`;
-        //     // return "@/assets/images/posts/1.jpg";
-        // },
         getRandomImage(ext) {
-            const randomNumber = this.getRandomNumber(1, 2);
+            const randomNumber = this.getRandomNumber(1, 25);
             return require(`@/assets/images/posts/${randomNumber}.${ext}`);
         },
     }
 }
 </script>
+
+<style scoped>
+/* #post {
+    height: 100%;
+} */
+.thumbnail {
+    width: 100%;
+    height: 200px;
+    background-size: cover;
+    background-position: center center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    /* margin: 10px 0; */
+    color: #fff;
+}
+
+.card-footer {
+    height: 110px;
+}
+
+@media only screen and (max-width: 550px) {
+    .card-footer {
+        height: 120px;
+    }
+}
+</style>
